8adac0e8bf6d93db29732dbd16283f6d859a7d6d
Jura Slapnicka <jiri.slapnicka@gmail.com>
2022-11-01 14:28:20 +0100

    create new model when load false

--- train.py
+++ train.py
@@ -435,10 +435,12 @@ if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     logging.info(f'Using device {device}')
 
-    model = Yolov4(cfg.pretrained, n_classes=cfg.classes)
     if cfg.load:
         print(f'Loading saved model from: {cfg.load}')
-        torch.load(cfg.load)
+        model = torch.load(cfg.load)
+    else:
+        print('Creating new model')
+        model = Yolov4(cfg.pretrained, n_classes=cfg.classes)
 
     if torch.cuda.device_count() > 1:
         model = torch.nn.DataParallel(model)
