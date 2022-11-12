ff37884230fdd7601cf222035da303617992bf03
Jura Slapnicka <jiri.slapnicka@gmail.com>
2022-11-12 15:49:27 +0100

    result img dif name

--- tool/utils.py
+++ tool/utils.py
@@ -326,6 +326,7 @@ def plot_boxes_cv2(img, boxes, savename=None, class_names=None, color=None):
                 rgb = (red, green, blue)
             img = cv2.putText(img, class_names[cls_id], (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1.2, rgb, 1)
         img = cv2.rectangle(img, (x1, y1), (x2, y2), rgb, 1)
+        savename = 'result_' + savename
     if savename:
         print("save plot results to %s" % savename)
         cv2.imwrite(savename, img)
@@ -369,6 +370,7 @@ def plot_boxes(img, boxes, savename=None, class_names=None):
             font = ImageFont.truetype("/usr/share/fonts/truetype/liberation/LiberationSans-Bold.ttf", font_size)
             draw.text((x1, y1), text, fill=rgb, font=font)
         draw.rectangle([x1, y1, x2, y2], outline=rgb)
+        savename = 'result_' + savename
     if savename:
         print("save plot results to %s" % savename)
         img.save(savename)
