871f36bf7aee0f9dd2aa6a1db989ca20bacdc067
Jura Slapnicka <jiri.slapnicka@gmail.com>
2022-11-19 15:55:37 +0100

    print model output size

--- tool/utils.py
+++ tool/utils.py
@@ -425,7 +425,8 @@ def do_detect(model, img, conf_thresh, n_classes, nms_thresh, use_cuda=1):
     list_features = model(img)
     print('-' * 99)
     print(f'list_features len: {len(list_features)}')
-    print(f'list_features shape: {list_features[0].size()}')
+    for feature in list_features:
+        print(f'feature shape: {feature.size()}')
     # print(f'image_path: {image_path}')
     # print(f'images: {images.size()}')
     # print(f'bboxes: {bboxes.size()}')
